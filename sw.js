2ed7495b599b1ef6c76c8f8b541c11f93bb43817
projectwanza <project.wanzamarketing@gmail.com>
2026-06-15 11:47:31 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "ar-connect-cache-v1";
+const urlsToCache = [
+  "/Online-Store/",
+  "/Online-Store/index.html",
+  "/Online-Store/manifest.json",
+  "/Online-Store/icon-192.png",
+  "/Online-Store/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
